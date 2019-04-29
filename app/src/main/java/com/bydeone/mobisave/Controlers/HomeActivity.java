557f9a54932ac4bc69b21669308f7de46b537b13
patrick <playbydeone@gmail.com>
2019-04-29 17:53:15 +0100

    Creation de Home activity

---
+++ app/src/main/java/com/bydeone/mobisave/Controlers/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.bydeone.mobisave.Controlers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bydeone.mobisave.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home2);
+    }
+}
